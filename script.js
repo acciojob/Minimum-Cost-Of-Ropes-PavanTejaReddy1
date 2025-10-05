4a0a6658b22d0464194f461a9fbce30c188308c4
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-10-05 22:59:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+const result = document.getElementById("result");
+
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+	const input = document.getElementById("rope-lengths");
+	const arr = input.value.split(",").map(Number);
+	arr.sort((a,b) => a-b);
+	console.log(arr);
+
+	let totalCost = 0;
+
+	while(arr.length > 1) {
+		let firstValue = arr.shift();
+		let secondValue = arr.shift();
+
+		arr.push(firstValue + secondValue);
+		totalCost += firstValue + secondValue;
+
+		arr.sort((a,b) => a-b);
+	}
+
+	result.innerText = totalCost;
 }  
